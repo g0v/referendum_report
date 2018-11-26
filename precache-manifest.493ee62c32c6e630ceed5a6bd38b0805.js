59effd08e358dff276899c853adbf051a9169102
YuTin <yuting1987@gmail.com>
2018-11-26 17:13:28 +0800

    Updates

---
+++ precache-manifest.493ee62c32c6e630ceed5a6bd38b0805.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6289b1d6b1c1a558daa",
+    "url": "/referendum_report/static/js/runtime~main.e6289b1d.js"
+  },
+  {
+    "revision": "544213cbeb6040d46c01",
+    "url": "/referendum_report/static/js/main.544213cb.chunk.js"
+  },
+  {
+    "revision": "e4960c77307584f09f5d",
+    "url": "/referendum_report/static/js/1.e4960c77.chunk.js"
+  },
+  {
+    "revision": "544213cbeb6040d46c01",
+    "url": "/referendum_report/static/css/main.3ec18ee8.chunk.css"
+  },
+  {
+    "revision": "e967537fd11f184a9d4587fa6ba097e0",
+    "url": "/referendum_report/index.html"
+  }
+];
